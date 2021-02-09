472fcbfb5d5d3650afda3ca5e7a6ef983ecd6fa4
Manan <mehta.manan@aol.com>
2021-02-09 23:10:56 +0530

    init

---
+++ Casting_and_static_cast.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+
+class My_Integer
+{
+private:
+    int integer;
+
+public:
+    My_Integer(int value) : integer(value)
+    {
+        std::cout << "Conversion Constructor" << std::endl;
+    }
+
+    operator std::string()
+    {
+        std::cout << "Conversion Operator" << std::endl;
+        return std::to_string(integer);
+    }
+};
+
+// ----------------------------------------------------
+
+class A
+{
+};
+
+class B : private A
+{
+};
+
+int main()
+{
+    // C style cast
+
+    //int to double implicit
+    int a = 5;
+    double b = a;
+
+    // double to int implicit
+    double c = 5.25;
+    int d = c;
+
+    // double to int explicit - safe style cast
+    double e = 5.56;
+    int f = (int)e + 5.67;
+
+    // -----------------------------------------------------------------------------------------
+
+    // C++ style cast
+
+    double g = 3.89;
+    int h = static_cast<int>(g) + 5.67;
+
+    // ----------------------------------------------------
+
+    // 1. static_cast<type>(expression)
+    char my_char = 'a';
+    // C style no type compatibility checking
+    int *int_ptr = (int *)&my_char;
+    *int_ptr = 10; // run-time error
+    // C++ static cast
+    // int *int_ptr_2 = static_cast<int *>(&c); // compile error: invalid type conversion
+
+    // ----------------------------------------------------
+
+    My_Integer my_integer(5);
+    my_integer = 10;                          // Conversion ctor called
+    my_integer = static_cast<My_Integer>(10); // alternative way
+
+    std::string str = my_integer;               // Conversion operator called
+    str = static_cast<std::string>(my_integer); // alternative way
+
+    // ----------------------------------------------------
+
+    // Converting to and from void*
+    int i = 20;
+    void *void_ptr = static_cast<void *>(&i);
+    int *int_ptr_ = static_cast<int *>(void_ptr);
+
+    // ----------------------------------------------------
+
+    B base;
+    A *a_ptr = (A *)(&base);              // legal
+    // A *a_ptr_2 = static_cast<A *>(&base); // error: conversion to inaccessible base class "A" is not allowed ... works only with public inheritance
+}
---
+++ Iterators.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+int main()
+{
+    std::vector<int> values = {1, 2, 3, 4, 5};
+
+    // traditional loop to iterate over the collection
+    // not a cleaner code as i is kind of redundant
+    // all data structures are not linear like sets, maps, trees,.. hence we need iterators and range based loops for them
+    for (int i = 0; i < values.size(); i++)
+    {
+        std::cout << values[i] << std::endl;
+    }
+
+    // range based for loop since C++11
+    // cleaner code
+    // this code works as vector class provides implementation for begin and end value and returns and iterator
+    // read-only
+    // cant change begin and end of collection
+    // short-hand for below code
+    for (int value : values)
+    {
+        std::cout << value << std::endl;
+    }
+
+    // manipulate the iterator position and collection values - read +  write
+    // explicit begin and end
+    for (std::vector<int>::iterator it = values.begin(); it != values.end(); it++)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    std::unordered_map<std::string, int> map;
+    map["Cherno"] = 5;
+    map["C++"] = 2;
+
+    using ScoreMap = std::unordered_map<std::string, int>;
+    using ScoreMapConstiter = ScoreMap::const_iterator;
+
+    for (ScoreMapConstiter it = map.begin(); it != map.end(); it++)
+    {
+        // std::pair <string, int>
+        auto&  key = it->first; // ->
+        auto& value = it->second;
+        std::cout << key << " " << value << std::endl;
+    }
+
+    // range loop for above
+    for( auto kv : map)
+    {
+        auto&  key = kv.first; //.
+        auto& value = kv.second;
+        std::cout << key << " " << value << std::endl;
+    }
+
+    // more cleaner c++17
+    for (auto& [key, value] : map)
+    {
+        std::cout << key << " " << value << std::endl;
+    }
+    std::cin.get();
+}
