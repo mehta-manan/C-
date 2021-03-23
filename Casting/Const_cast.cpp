2de5151b172e400169f751779c91d78e3c639065
Manan <mehta.manan@aol.com>
2021-03-23 23:18:22 +0530

    init

---
+++ Casting/Const_cast.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int func(int *ptr)
+{
+    return ++(*ptr);
+}
+
+int main()
+{
+    const int *int_ptr = new int(10);
+    std::cout << "*int_ptr: " << *int_ptr << std::endl;
+
+    // error: (*int_ptr)++;
+
+    int *int_ptr_2 = const_cast<int *>(int_ptr);
+    std::cout << "*int_ptr_2: " << *int_ptr << std::endl;
+
+    (*int_ptr_2)++;
+    std::cout << "*int_ptr_2: " << *int_ptr << std::endl;
+
+    // --------------------------------------------------------
+
+    const int a = 20;
+    const int *int_ptr_3 = &a;
+    int *int_ptr_4 = const_cast<int *>(int_ptr_3);
+
+    // --------------------------------------------------------
+
+    const int *int_ptr_5 = new int(30);
+    std::cout << "func(): " << func(const_cast<int *>(int_ptr_5));
+}
