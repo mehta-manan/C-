1117933ee0c59ac3787eda83c463d817fff0776a
mehta-manan <mehta.manan@aol.com>
2021-03-31 23:27:17 +0530

    update

--- CLI_arguments.cpp
+++ CLI_arguments.cpp
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     std::cout << "Arguments passed: " << std::endl;
 
     for (int i = 0; i < argc; i++)
-        std::cout << argv[i] << " " << std::endl;
+        std::cout << argv[i] << std::endl;
 }
 
 /*
